965ffd6ef35a559528edfefc86f7e55761ee1cc9
vivdto <vivdto@gmail.com>
2024-07-30 01:54:48 +0530

    Added new program!

--- Recursion/removeDuplicatefromString.cpp
+++ Recursion/removeDuplicatefromString.cpp
@@ -1,17 +1,17 @@
-//Remove Duplicates
-void removeDuplicates(string str, int i, bool map[26], string ans) {
-    if(i == str.length()) {
-        cout << "ans : " << ans << endl;
-        return;
-    }
+// //Remove Duplicates
+// void removeDuplicates(string str, int i, bool map[26], string ans) {
+//     if(i == str.length()) {
+//         cout << "ans : " << ans << endl;
+//         return;
+//     }
 
-    int mapIdx = (int)(str.at(i) - 'a');
+//     int mapIdx = (int)(str.at(i) - 'a');
 
-    if(map[mapIdx]) {
-        removeDuplicates(str, i+1, map, ans);
-    } else {
-        map[mapIdx] = true;
-        removeDuplicates(str, i+1, map, ans+str.at(i));
-    }
+//     if(map[mapIdx]) {
+//         removeDuplicates(str, i+1, map, ans);
+//     } else {
+//         map[mapIdx] = true;
+//         removeDuplicates(str, i+1, map, ans+str.at(i));
+//     }
     
-}
+// }
